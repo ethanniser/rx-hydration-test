7af5eec74ac1af9a569e7433190e8af6e222e542
Ethan Niser <ethan@niser.com>
2025-07-16 22:44:22 -0700

    things are setup

---
+++ src/app/client.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { Rx, useRx, useRxValue } from "@effect-rx/rx-react";
+import { Effect, Schema } from "effect";
+import { countRx, testRx } from "./rx";
+
+export function Client() {
+  const [count, setCount] = useRx(countRx);
+  const test = useRxValue(testRx);
+
+  return (
+    <div className="card">
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <pre>{JSON.stringify(test, null, 2)}</pre>
+    </div>
+  );
+}
---
+++ src/app/rx.ts
@@ -0,0 +1,16 @@
+import { Rx } from "@effect-rx/rx-react";
+import { Effect } from "effect";
+
+export const countRx = Rx.make(0);
+
+export const testRx = Rx.make(
+  Effect.gen(function* () {
+    yield* Effect.log("starting test");
+    yield* Effect.sleep("2 seconds");
+    yield* Effect.log("done");
+    return {
+      test: Date.now(),
+    };
+  })
+);
+// ).pipe(Rx.serializable(Schema.Number));
